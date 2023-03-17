f84b48321fdc3052c155abfe15a6bd2bf068354e
aj sth <aj.sth.2020@gmail.com>
2023-03-17 13:56:03 +0545

    added response interface

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,8 +2,32 @@ import Head from "next/head";
 import Main from "../src/components/Main.jsx";
 var seedrandom = require("seedrandom");
 
-export default function Home({data}) {
-	console.log(data)
+interface Response {
+  data: {
+    id: number;
+    title: string;
+    thumbnail: {
+			lqip: string;
+			width: number;
+			height: number;
+			alt_text: string;
+		}
+    date_display: string;
+    artist_display: string;
+  };
+	info: {
+		license_text: string;
+		license_links: any;
+		version: string;
+	}
+	config: {
+		iiif_url: string;
+		website_url: string;
+	}
+}
+
+export default function Home({ data }: Response) {
+  console.log(data);
   return (
     <>
       <Head>
@@ -12,25 +36,25 @@ export default function Home({data}) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Main data={ data }></Main>
+      <Main data={data}></Main>
     </>
   );
 }
 
 // This gets called on every request
 export async function getServerSideProps() {
-	// generate random art id based on today's date
+  // generate random art id based on today's date
   let dateString = new Date().toDateString();
   var myrng = seedrandom(dateString);
   let artID = Math.floor((myrng() * 1000000) % 119560);
 
   // make api call
-  let daily_link = `https://api.artic.edu/api/v1/artworks/${artID}`;
+  let daily_link = `https://api.artic.edu/api/v1/artworks/${artID}/?fields=id,title,date_display,artist_display,thumbnail`;
 
   // Fetch data from external API
-  const res = await fetch(daily_link)
-  const data = await res.json()
+  const res = await fetch(daily_link);
+  const data = await res.json();
 
   // Pass data to the page via props
-  return { props: { data } }
+  return { props: { data } };
 }
